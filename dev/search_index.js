168735debcb900459f1ad128290547bfa08f8e60
Documenter.jl <documenter@juliadocs.github.io>
2024-12-19 03:36:06 +0000

    build based on 870e437

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ferromodo","category":"page"},{"location":"#Ferromodo","page":"Home","title":"Ferromodo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ferromodo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ferromodo]","category":"page"}]
+}
